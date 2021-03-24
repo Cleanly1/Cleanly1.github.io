b2ace9d212912a231fa1124d107071b836c72822
Cleanly <oskar.turesson99@gmail.com>
2021-03-24 20:22:53 +0100

    Updates

---
+++ precache-manifest.489ae7519f557cec4579f8555848e592.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed1660a0410b1c548818950a85b857d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dca6390388939b4e66a9",
+    "url": "/static/css/main.cdbf0b5a.chunk.css"
+  },
+  {
+    "revision": "b5868115f074e5a8bf1b",
+    "url": "/static/js/2.8f9ac2d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8f9ac2d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dca6390388939b4e66a9",
+    "url": "/static/js/main.44186aea.chunk.js"
+  },
+  {
+    "revision": "9f4e128544f587e2b66e",
+    "url": "/static/js/runtime-main.5f76594a.js"
+  }
+]);
