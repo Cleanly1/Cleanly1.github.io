7404a64c0eca6bd295b038f632acd901284fec55
Cleanly <oskar.turesson99@gmail.com>
2021-05-18 21:05:26 +0200

    moved to use jsx instead of js

--- src/pages/SingleProject/index.jsx
+++ src/pages/SingleProject/index.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import { richTextToReact } from "../../utils/contentful";
 import { Container, Box } from "../../components";
 import { getTechString } from "../../utils/functions";
+import { Bold, Text } from "../../utils/richtext"
 import { BLOCKS, MARKS, INLINES } from "@contentful/rich-text-types";
 
 function SingleProject({ location, id }) {
   const data = location.state.data.fields;
 
-  const Bold = ({ children }) => <em class="bold">{children}</em>;
-
-  const Text = ({ children }) => <p>{children}</p>;
 
   const options = {
     renderMark: {
@@ -26,6 +24,7 @@ function SingleProject({ location, id }) {
 
     // renderText: (text) => text.replace(".", "?"),
   };
+
 
   return (
     <Container>
