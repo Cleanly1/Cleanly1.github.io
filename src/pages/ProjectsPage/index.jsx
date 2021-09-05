10baf590df91adf9d43c5b8f68487e683dc0b89b
Cleanly <oskar.turesson99@gmail.com>
2021-09-05 22:57:33 +0200

    implemented ProjectsList component

--- src/pages/ProjectsPage/index.jsx
+++ src/pages/ProjectsPage/index.jsx
@@ -1,25 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { fetchEntriesByType } from "../../utils/contentful";
-import { Container, Project, Box } from "../../components";
+import React from "react";
+import { Container, Box, ProjectsList } from "../../components";
 
 function ProjectsPage() {
-  const [projects, setProjects] = useState(false);
-  useEffect(() => {
-    const fetchData = async () => {
-      const projectList = await fetchEntriesByType("project");
-      setProjects(projectList);
-    };
-    fetchData();
-  }, []);
   return (
     <Container>
       <Box>
         <h1>All Projects:</h1>
       </Box>
-      {projects.length > 0 &&
-        projects.map((project, i) => {
-          return <Project key={i} data={project} />;
-        })}
+      <ProjectsList />
     </Container>
   );
 }
