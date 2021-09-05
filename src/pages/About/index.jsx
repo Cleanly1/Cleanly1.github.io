841ea2e484654d5175d56ce910be707b11f518f7
Cleanly <oskar.turesson99@gmail.com>
2021-09-05 23:07:45 +0200

    added fetch of data from contentful

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchPageBySlug, richTextToReact} from '../../utils/contentful'
 import { Container, Box } from "../../components";
 
 function About() {
+  const [info, setInfo] = useState(false);
+  const [loading, setLoading] = useState(true)
+
+  useEffect(()=>{
+    const getData = async () => {
+      const data = await fetchPageBySlug('about-me');
+      setInfo(data.items[0].fields)
+      setLoading(false)
+    }
+    getData()
+
+  }, [])
+  console.log(info);
+
+  if (loading) {
+    return (<Container><Box>Loading....</Box></Container>)
+  }
+
   return (
     <Container>
       <Box>
         <h1>About me</h1>
+        <h3>{info.headerTitle}</h3>
+
       </Box>
+        {info.firstSection !== undefined && (<Box>{richTextToReact(info.firstSection)}</Box>)}
+        {info.secondSection !== undefined && (<Box>{richTextToReact(info.firstSection)}</Box>)}
     </Container>
   );
 }
