8240582a41ee4a120b6a2d755d764f9316f10e64
Cleanly <oskar.turesson99@gmail.com>
2021-09-05 22:56:27 +0200

    removed fetching of projects in Home component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,30 +1,19 @@
-import React, { useEffect, useState } from "react";
-import { fetchFeaturedProjects } from "../../utils/contentful";
-import { Container, Quote, Project, Box } from "../../components";
+import React from "react";
+import { Container, Quote, Box, ProjectsList, Socials } from "../../components";
 
 function Home() {
-  const [projects, setProjects] = useState(false);
-  useEffect(() => {
-    const fetchData = async () => {
-      const featuredList = await fetchFeaturedProjects();
-      setProjects(featuredList);
-    };
-    fetchData();
-  }, []);
   return (
     <Container>
       <Box>
         <h1>Hello!</h1>
         <h1>My name is Oskar Turesson.</h1>
         <p>I am a web developer with fullstack tendencies</p>
+        <Socials to="https://github.com/Cleanly1" src="assets/images/github.png"/>
       </Box>
       <Box>
         <h1>Featured Projects:</h1>
       </Box>
-      {projects.length > 0 &&
-        projects.map((project, i) => {
-          return <Project key={i} data={project} />;
-        })}
+      <ProjectsList featured/>
       <Quote />
     </Container>
   );
