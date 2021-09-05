03090433458cda8d446e5930507284c92b80b7ef
Cleanly <oskar.turesson99@gmail.com>
2021-09-05 22:59:53 +0200

    cleaned upp file a bit

--- src/pages/SingleProject/index.jsx
+++ src/pages/SingleProject/index.jsx
@@ -1,10 +1,22 @@
 import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 import { useParams } from "react-router-dom";
 import { richTextToReact, fetchProjectsBySlug } from "../../utils/contentful";
 import { Container, Box } from "../../components";
 import { getTechString } from "../../utils/functions";
-import { Bold, Text } from "../../utils/richtext"
+import { Bold, Text, Hyper } from "../../utils/richtext"
 import { BLOCKS, MARKS, INLINES } from "@contentful/rich-text-types";
+
+const StyledImg = styled.img`
+  margin: 1rem 0;
+  max-height: 20rem;
+  max-width: 100%;
+
+  @media (min-width: 1024px) {
+    height: 100%;
+    max-width: inherit;
+  }
+`;
 
 function SingleProject(){
   const [data, setData] = useState()
@@ -20,19 +32,16 @@ function SingleProject(){
     if (data === undefined) {
       fetchData()
     }
-
-
   }, [setData, slug, data])
+
   const options = {
     renderMark: {
-      [MARKS.BOLD]: (text) => <Bold>{text}</Bold>,
+      [MARKS.BOLD]: (text) => <Bold>{ text }</Bold>,
     },
     renderNode: {
-      [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
+      [BLOCKS.PARAGRAPH]: (node, children) => <Text>{ children }</Text>,
       [INLINES.HYPERLINK]: (node, children) => (
-        <a href={node.data.uri} target="_blank" rel="noopener noreferrer">
-          {children}
-        </a>
+        <Hyper node={node}>{ children }</Hyper>
       ),
     },
 
@@ -40,18 +49,24 @@ function SingleProject(){
   };
 
   if (loading) {
-    return (<Container>
-      <h1>loading...</h1>
+    return (
+    <Container>
+      <h1>Loading...</h1>
     </Container>)
   }
 
   return (
     <Container>
       <Box>
-        <h1>The Project</h1>
-      </Box>
-      <Box>
         <h1>{data.title}</h1>
+      </Box>
+
+      <StyledImg
+        src={data.img.fields.file.url + "?w=800"}
+        alt={data.img.fields.file.description}
+      />
+
+      <Box>
         <div>{richTextToReact(data.desc, options)}</div>
       </Box>
       <Box>Tech: {getTechString(data.tech)}</Box>
