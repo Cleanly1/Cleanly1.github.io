f1c74fed6ebe96f2514d65c4af80d55d911b3650
Cleanly <oskar.turesson99@gmail.com>
2021-09-05 23:06:29 +0200

    added richtext link component

--- src/utils/richtext.js
+++ src/utils/richtext.js
@@ -1,5 +1,16 @@
 import React from "react";
+import styled from "styled-components";
+
+const Link = styled.a`
+  color: white;
+`;
 
 export const Bold = ({ children }) => <em class="bold">{children}</em>;
 
 export const Text = ({ children }) => <p>{children}</p>;
+
+export const Hyper = ({ node, children }) => (
+  <Link href={node.data.uri} target="_blank" rel="noopener noreferrer">
+    {children}
+  </Link>
+);
